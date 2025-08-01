ba360030e9fcfdea3e69b75d6ec5cca1db1a1200
Natalie Shagug <natysh343@gmail.com>
2025-08-01 15:42:47 +0300

    First version of Streamlit app
    Basic version with working weather search, User can enters a city name and gets weather info.

---
+++ app.py
@@ -0,0 +1,14 @@
+import requests
+import streamlit as st
+from main import get_weather_data
+
+
+st.title("Weather Checker App")
+city_name = st.text_input("Enter City Name : ")
+if st.button("Check Weather"):
+    if city_name:
+        weather_data = get_weather_data(city_name)
+        st.text(weather_data)
+    else:
+        st.warning("Please check the city name.")
+
