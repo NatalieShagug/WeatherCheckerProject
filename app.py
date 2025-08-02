22c0ec9efcbb9cd045bee7d039706d23f83f83b5
Natalie Shagug <natysh343@gmail.com>
2025-08-02 15:23:38 +0300

    Add subtitle and display weather using st.code
    dded a short subtitle to explain what the app does and changed st.text to st.code to make the weather data easier to read.

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 import streamlit as st
 from main import get_weather_data
 
-
 st.title("Weather Checker App")
+st.markdown("Check the current weather in any city")
 city_name = st.text_input("Enter City Name : ")
 if st.button("Check Weather"):
     if city_name:
         weather_data = get_weather_data(city_name)
-        st.text(weather_data)
+        st.code(weather_data)
     else:
+        st.warning("Please check the city name.")
